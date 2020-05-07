4fa70bbaf2aadf0ee27493208f1157729cb88108
Mark Barnes <msbarnes@ualr.edu>
2020-05-07 14:35:11 -0500

    update to sheet music generation

--- app.py
+++ app.py
@@ -85,9 +85,9 @@ def midi_dl():
 @login_required
 def sheet_dl():
     stream = midi.translate.midiFilePathToStream(request.form['filename'])
-    sheet = stream.write('%s.pdf' % request.form['filename'])
+    sheet = stream.write('lily.pdf')
     #return send_file(l, as_attachment=True)
-    return send_file('%s.pdf' % request.form['filename'], as_attachment=True)
+    return send_file(sheet, as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
